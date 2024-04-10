c7cb1c3a10f9c8d513358d52ef1df2ae6cdea3f0
topittipF <158184004+topittipF@users.noreply.github.com>
2024-04-10 07:10:42 +0000

    comit new changes

--- hello.py
+++ hello.py
@@ -1,15 +1,1 @@
-def find_smallest_number():
-    # Prompt the user to input three numbers
-    num1 = float(input("1 "))
-    num2 = float(input("10 "))
-    num3 = float(input("200 "))
-
-    # Finding the smallest number
-    smallest = num1
-    if num2 < smallest:
-        smallest = num2
-    if num3 < smallest:
-        smallest = num3
-
-    # Print the smallest number
-    print("The smallest number is:", smallest)
+print(hello=World)
